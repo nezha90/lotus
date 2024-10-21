3113a998bb31a1a9ae18d3eb7a1057638a413184
nezha <nezhayoung@outlook.com>
2024-10-21 15:22:04 +0800

    tmp

--- cmd/lotus-wallet/rpc.go
+++ cmd/lotus-wallet/rpc.go
@@ -28,7 +28,7 @@ func methodFilterMiddleware(next http.Handler) http.Handler {
 		// 读取请求体
 		body, err := io.ReadAll(r.Body)
 		if err != nil {
-			http.Error(w, "Failed to read request body", http.StatusBadRequest)
+			http.Error(w, "1 Failed to read request body", http.StatusBadRequest)
 			return
 		}
 		defer r.Body.Close()
@@ -36,9 +36,10 @@ func methodFilterMiddleware(next http.Handler) http.Handler {
 		// 解析 JSON-RPC 请求
 		var req rpcRequest
 		if err := json.Unmarshal(body, &req); err != nil {
-			http.Error(w, "Invalid JSON-RPC request", http.StatusBadRequest)
+			http.Error(w, "2 Invalid JSON-RPC request", http.StatusBadRequest)
 			return
 		}
+
 		// 检查 method 字段是否为允许的 RPC 方法
 		switch req.Method {
 		case MethodWalletBalance, MethodWalletList:
@@ -47,7 +48,7 @@ func methodFilterMiddleware(next http.Handler) http.Handler {
 			handleWalletSign(w, req.Params)
 		default:
 			// 不支持的 method 返回错误
-			http.Error(w, fmt.Sprintf("Unsupported method: %s", req.Method), http.StatusBadRequest)
+			http.Error(w, fmt.Sprintf("3 Unsupported method: %s", req.Method), http.StatusBadRequest)
 			return
 		}
 
@@ -62,58 +63,55 @@ func handleWalletSign(w http.ResponseWriter, rawParams json.RawMessage) {
 	// 解析 WalletSign 的 params
 	var params []interface{}
 	if err := json.Unmarshal(rawParams, &params); err != nil {
-		http.Error(w, "Invalid params", http.StatusBadRequest)
+		http.Error(w, "4 Invalid params", http.StatusBadRequest)
 		return
 	}
 
 	// 校验参数长度
 	if len(params) != 3 {
-		http.Error(w, "Invalid number of params", http.StatusBadRequest)
+		http.Error(w, "5 Invalid number of params", http.StatusBadRequest)
 		return
 	}
 	var msgMeta api.MsgMeta
 	msgMetaParamsBytes, err := json.Marshal(params[0]) // 参数列表中第三个参数为消息对象
 	if err != nil {
-		http.Error(w, "Failed to parse sign params", http.StatusBadRequest)
+		http.Error(w, "6 Failed to parse sign params", http.StatusBadRequest)
 		return
 	}
 
 	if err := json.Unmarshal(msgMetaParamsBytes, &msgMeta); err != nil {
-		http.Error(w, "Failed to parse sign message", http.StatusBadRequest)
+		http.Error(w, "7 Failed to parse sign message", http.StatusBadRequest)
 		return
 	}
 
 	if msgMeta.Type == api.MTBlock {
 		return
 	} else if msgMeta.Type != api.MTChainMsg {
-		http.Error(w, "Failed to sign this type", http.StatusBadRequest)
+		http.Error(w, "8 Failed to sign this type", http.StatusBadRequest)
 	}
 
-	//
 	var addr address.Address
 	var msgByte []byte
 
 	addrParamsBytes, err := json.Marshal(params[0]) // 参数列表中第三个参数为消息对象
 	if err != nil {
-		http.Error(w, "Failed to parse sign params", http.StatusBadRequest)
+		http.Error(w, "9 Failed to parse sign params", http.StatusBadRequest)
 		return
 	}
 
 	if err := json.Unmarshal(addrParamsBytes, &addr); err != nil {
-		http.Error(w, "Failed to parse sign message", http.StatusBadRequest)
+		http.Error(w, "10 Failed to parse sign message", http.StatusBadRequest)
 		return
 	}
 
 	msgByte, err = json.Marshal(params[0]) // 参数列表中第三个参数为消息对象
 	if err != nil {
-		http.Error(w, "Failed to parse sign params", http.StatusBadRequest)
+		http.Error(w, "11 Failed to parse sign params", http.StatusBadRequest)
 		return
 	}
 	fmt.Println(msgByte)
 	fmt.Println(addr)
-	//
-	//
-	//
+
 	//// 记录 from, to 和消息类型
 	//fmt.Printf("Signing from: %s, to: %s, message type: %s\n", signParams.From, signParams.To, signParams.Msg.MessageType)
 	//
